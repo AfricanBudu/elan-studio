e990c5c1d2c2bd3e22f60f5b4fcd30cf24e31ced
AfricanBudu <abdullahagboola202@gmail.com>
2025-05-22 15:14:22 +0100

    add navItems to constants

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Pages",
+    url: "/pages",
+  },
+  {
+    name: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Pricing",
+    url: "/pricing",
+  },
+];
